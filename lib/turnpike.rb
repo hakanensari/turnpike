fb9b8e57fd9cf7ea991f8bf29e6502bc0682c49a
hakanensari <hakan.ensari@papercavalier.com>
2013-05-21 16:15:31 +0100

    Add namespace setter

--- lib/turnpike.rb
+++ lib/turnpike.rb
@@ -3,9 +3,13 @@ require 'turnpike/unique_queue'
 
 module Turnpike
   class << self
+    attr_accessor :namespace
+
     def call(name = 'default', unique: false)
       (unique ? UniqueQueue : Queue).new(name)
     end
     alias [] call
   end
+
+  @namespace = 'turnpike'
 end
